962686ad4fa4940c36ef52721921c1855f47861a
mrvemavampire-byte <mrvemavampire@gmail.com>
2026-05-27 17:49:53 +0600

    Add server.js

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let games = {};
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('joinGame', (gameId) => {
+        socket.join(gameId);
+        if (!games[gameId]) {
+            games[gameId] = {
+                players: [],
+                board: null
+            };
+        }
+        
+        if (games[gameId].players.length < 2) {
+            games[gameId].players.push(socket.id);
+            const color = games[gameId].players.length === 1 ? 'w' : 'b';
+            socket.emit('playerAssignment', { color });
+        } else {
+            socket.emit('fullGame', 'Game is full');
+        }
+
+        if (games[gameId].players.length === 2) {
+            io.to(gameId).emit('gameStart');
+        }
+    });
+
+    socket.on('move', ({ gameId, move }) => {
+        socket.to(gameId).emit('move', move);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+        // Cleanup game logic could go here
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
